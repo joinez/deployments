9f282f22089846f8f0dfcd104fad209f37fcccb1
joinez <matthias@hinterschwepfinger.net>
2019-06-20 18:22:41 +0200

    Record test results in circleci

--- script/circleci-setup.sh
+++
@@ -1,6 +0,0 @@
-cd backend
-gem install bundler --conservative
-bundle install --with development test --without production --jobs 4 --no-cache --retry 3
-RAILS_ENV=test rails db:create db:migrate
-rails log:clear tmp:clear
-cd -
