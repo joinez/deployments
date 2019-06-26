58e895494894834a71a4aba5caba811576164c0b
joinez <matthias@hinterschwepfinger.net>
2019-06-26 23:38:24 +0200

    Redirect to graphiql ui via route, remove controller

--- backend/app/controllers/frontend_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class FrontendController < ActionController::API
-  def redirect
-    redirect_to ENV.fetch('FRONTEND_URL', 'http://localhost:8080')
-  end
-end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root 'frontend#redirect'
-
   post '/graphql', to: 'graphql#execute'
 
   if Rails.env.development?
+    get '/' => redirect('/graphiql')
     mount GraphiQL::Rails::Engine,
           at: '/graphiql',
           graphql_path: 'graphql#execute'
