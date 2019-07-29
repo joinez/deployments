8dfb0b0eb58b6200864a3badbf0190b640da5a40
joinez <matthias@hinterschwepfinger.net>
2019-07-29 17:26:36 +0200

    Add production build for frontend
    - Allow cors in backend for now

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('FRONTEND_URL', 'http://localhost:8080')
-    resource '*', headers: :any, methods: :post
+    origins '*'
+    resource '*', headers: :any, methods: :any
   end
 end
